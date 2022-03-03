137089b63b76b581eecca0b2d62797e87030ff36
hofe <ghcj19@naver.com>
2022-03-03 18:29:10 +0900

    update

--- SinglePerceptron.c
+++ SinglePerceptron.c
@@ -3,7 +3,8 @@
 #include <time.h>
 
 #define MAX_SIZE 2
-
+#define X_AXIS MAX_SIZE*10
+#define Y_AXIS MAX_SZIE*10
 typedef struct _perceptron_t
 {
     int x[MAX_SIZE];
@@ -14,16 +15,14 @@ typedef struct _perceptron_t
 pct_t *init_pct(pct_t *pct);
 pct_t *set_param(pct_t *pct, int wight, int bias);
 
-int and_gate(data_t *data);
-int nand_gate(data_t *data);
-int or_gate(data_t *data);
+int gate(pct_t *pct, int wight, int bias);
 
 pct_t pct[1];
 int main()
 {
-    int input[MAX_SIZE];
     init_pct(pct[0]);
-    and_gate
+    printf("%d",and_gate(pct[0],0.5,-0.7));
+     
     return 0;
 }
 
@@ -41,23 +40,44 @@ pct_t *init_pct(pct_t *pct)
 pct_t *set_param(pct_t *pct, int wight, int bias)
 {
     int i=0;
+    int num;
+
+    srand((unsigned int)time(NULL));
+    printf("input data ");
 
     for( i =0; i<MAX_SIZE; i++)
     {
 	pct->wight[i] = wight;
+	pct->x[i]= (int)rand()%10;
+	printf("%d",pcb->x[i]);
+   
     }
+    printf("\n");
     pct->bias = bias;
-
     return pct;
 }
 
-int and_gate(int *input, pct_t *pct, int wight, int bias)
+int gate(pct_t *pct, int wight, int bias)
 {
     int i=0;
     int y=0;
+    int result = -1;
     set_param(pct, wight, bias);
     for(i=0; i<MAX_SIZE; i++)
     {
-	y += data->x[i]*pct->wight[i];
+	y += pct->x[i]*pct->wight[i];
     }
+    y += pct->bias;
+    if( y <= 0)
+	return 0;
+    else
+	return 1;
+}
+
+int show_graph(pct, int wight, int bias)
+{
+    char x_axis[X_AXIS];
+    char y_axis[Y_AXIS];
+
+    return 0;
 }
