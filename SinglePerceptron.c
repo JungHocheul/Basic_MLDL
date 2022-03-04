03d4fc58417800a2aae96854a7c9b5285598e997
hofe <ghcj19@naver.com>
2022-03-04 16:48:27 +0900

    update

--- SinglePerceptron.c
+++ SinglePerceptron.c
@@ -4,25 +4,33 @@
 
 #define MAX_SIZE 2
 #define X_AXIS MAX_SIZE*10
-#define Y_AXIS MAX_SZIE*10
+#define Y_AXIS MAX_SIZE*10
 typedef struct _perceptron_t
 {
     int x[MAX_SIZE];
-    int wight[MAX_SIZE];
-    int bias;
+    double wight[MAX_SIZE];
+    double bias;
 }pct_t;
 
 pct_t *init_pct(pct_t *pct);
-pct_t *set_param(pct_t *pct, int wight, int bias);
+pct_t *set_param(pct_t *pct, double wight, double bias);
 
-int gate(pct_t *pct, int wight, int bias);
+int gate(pct_t *pct, double wight, double bias);
 
 pct_t pct[1];
 int main()
 {
-    init_pct(pct[0]);
-    printf("%d",and_gate(pct[0],0.5,-0.7));
-     
+    int ret, ret1, ret2;
+    init_pct(pct);
+    while(1)
+    {
+    ret = gate(pct, 0.5,-0.7);
+    ret1 = gate(pct, -0.5,0.7);
+    ret2 = gate(pct, 0.6,-0.5);
+    printf("and gate :: %d :::  nand gate %d :: or gate%d\n",ret, ret1,ret2);
+
+    sleep(1);
+    }
     return 0;
 }
 
@@ -37,7 +45,7 @@ pct_t *init_pct(pct_t *pct)
     return pct;
 }
 
-pct_t *set_param(pct_t *pct, int wight, int bias)
+pct_t *set_param(pct_t *pct, double wight, double bias)
 {
     int i=0;
     int num;
@@ -48,36 +56,30 @@ pct_t *set_param(pct_t *pct, int wight, int bias)
     for( i =0; i<MAX_SIZE; i++)
     {
 	pct->wight[i] = wight;
-	pct->x[i]= (int)rand()%10;
-	printf("%d",pcb->x[i]);
+	pct->x[i] = (int)rand()%10;
+	printf("%d",pct->x[i]);
    
     }
     printf("\n");
+    printf("Complete Setting param \n");
     pct->bias = bias;
     return pct;
 }
 
-int gate(pct_t *pct, int wight, int bias)
+int gate(pct_t *pct, double wight, double bias)
 {
     int i=0;
-    int y=0;
+    float y=0;
     int result = -1;
     set_param(pct, wight, bias);
     for(i=0; i<MAX_SIZE; i++)
     {
-	y += pct->x[i]*pct->wight[i];
+	y += (float)pct->x[i]*pct->wight[i];
     }
-    y += pct->bias;
+    y += (float)pct->bias;
     if( y <= 0)
 	return 0;
     else
 	return 1;
 }
 
-int show_graph(pct, int wight, int bias)
-{
-    char x_axis[X_AXIS];
-    char y_axis[Y_AXIS];
-
-    return 0;
-}
--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char* argv[])
 {
+    int num;
+    int num2;
+    int num3;
+    while(1)
+    {
+    srand((unsigned int)time(NULL));
+    num2 = rand();
+    num3 = (int)rand()%10;
+    printf("%d %d %d",num, num2, num3);
+    }
     return 0;
 }
